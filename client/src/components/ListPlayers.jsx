9f1be065fc44733bb69772a62e8d8093884880c8
nizar <nizarr.tbs@gmail.com>
2023-09-09 10:52:09 +0100

    Create a popup to confirm deleting a player

--- client/src/components/ListPlayers.jsx
+++ client/src/components/ListPlayers.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SubNav1 from "./SubNav1";
 import axios from 'axios'
-
+import { Modal, Button } from 'react-bootstrap'
 
 const ListPlayers = (props) => {
   const { pageActive, setPageActive, players, setPlayers, setManagePageActive } = props;
+  const [show, setShow] = useState(false)
+  const [namePlayerToDelete, setNamePlayerToDelete] = useState("")
+  const [idPlayerToDelete, setIdPlayerToDelete] = useState()
+
+  const handleClose = () => {
+    setShow(false)
+  }
 
   useEffect(() => {
     setPageActive("ListPage");
@@ -16,10 +23,17 @@ const ListPlayers = (props) => {
       })
   }, [setPageActive, setPlayers, setManagePageActive]);
 
-  const deleteOnePlayer = (playerId) => {
-    axios.delete("http://localhost:5000/api/players/" + playerId)
+  const handleClickDelete = (playerId, playerName) => {
+    setIdPlayerToDelete(playerId)
+    setNamePlayerToDelete(playerName)
+    setShow(true)
+  }
+
+  const deleteOnePlayer = () => {
+    axios.delete("http://localhost:5000/api/players/" + idPlayerToDelete)
       .then(res => {
-        setPlayers(players.filter(player => player._id !== playerId))
+        setPlayers(players.filter(player => player._id !== idPlayerToDelete))
+        setShow(false)
       })
       .catch(err => console.log(err))
   }
@@ -27,6 +41,20 @@ const ListPlayers = (props) => {
   return (
     <div>
       <SubNav1 pageActive={pageActive} />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="text-danger">Attention !!!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="fw-semibold">{`Are you sure you want to delete this player : ${namePlayerToDelete}`}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={deleteOnePlayer}>
+            Ok
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <table className="table table-light table-striped table-hover text text-center mt-3 border">
           <thead>
             <tr>
@@ -42,7 +70,7 @@ const ListPlayers = (props) => {
                   <td className='text-start border'>{ player.name }</td>
                   <td className='text-start border'>{ player.preferredPosition }</td>
                   <td className='border'>
-                  <button type="submit" className="btn btn-danger" onClick={ () => deleteOnePlayer(player._id) }>Delete</button>
+                  <button type="submit" className="btn btn-danger" onClick={ () => handleClickDelete(player._id, player.name) }>Delete</button>
                   </td>
                 </tr>
               ))
